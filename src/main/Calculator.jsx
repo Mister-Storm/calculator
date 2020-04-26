179d073c3c07d6eedfb9a7ba45777c45cd4861f8
Fernando Costa Leite <clfernando1979@hotmail.com>
2020-04-26 20:01:12 -0300

    implementing the calculatory logic

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -3,8 +3,17 @@ import './Calculator.css';
 import Button from '../Components/Button'
 import Display from '../Components/Display'
 
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0
+}
 export default class Calculator extends Component {
     
+    state = {...initialState};
+
     constructor(props) {
         super(props);
         this.clearMemory = this.clearMemory.bind(this);
@@ -13,22 +22,60 @@ export default class Calculator extends Component {
     }
 
     clearMemory() {
-        // TODO 
+        this.setState({...initialState}); 
 
     }
 
     setOperation(operation) {
-        // TODO
+        if(this.state.current === 0) {
+            this.setState({operation, current:1, clearDisplay:true});
+        } else {
+            const equals = operation === '=';
+            const currentOperation = this.state.operation;
+            const values = {...this.state.values};
+            values[0] = values[0] === 'Err!!!!' ? 0 : values[0];
+            switch (currentOperation) {
+                case '+' : values[0] = values[0] + values[1];
+                break;
+                case '-' : values[0] = values[0] - values[1];
+                break;
+                case '*' : values[0] = values[0] * values[1];
+                break;
+                default : values[0]= values[1] !== 0 ? values[0] / values[1] : 'Err!!!!';
+            }
+            values[1] = 0;
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: equals,
+                values
+            });
+        }
     }
 
     addDigit(num) {
-        // TODO
+        if(num ==='.' && this.state.displayValue.includes('.')) {
+            return;
+        }
+        const clearDisplay = ((this.state.displayValue==='0' && num !=='.') 
+        || this.state.displayValue==='Err!!!!'
+        || this.state.clearDisplay);
+        const currentValue = clearDisplay ? '' : this.state.displayValue;
+        const displayValue = currentValue + num;
+        this.setState({displayValue, clearDisplay:false});
+        if(num !== '.') {
+            const i = this.state.current;
+            const values = {...this.state.values};
+            values[i] = parseFloat(displayValue);
+            this.setState({values});
+        }
     }
 
     render () {
         return (
             <div className="calculator">
-                <Display value={56} />
+                <Display value={this.state.displayValue} />
                 <Button label ="AC" click={this.clearMemory} triple />
                 <Button label ="/" click={this.setOperation} operation/>
                 <Button label ="7" click={this.addDigit} />
